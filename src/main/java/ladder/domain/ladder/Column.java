bdaedbfa5672554923f46f7e84ca6078b4b7a566
nokchax <nokchax@gmail.com>
2020-05-27 01:20:19 +0900

    feat : column's next column generate test and implement

--- src/main/java/ladder/domain/ladder/Column.java
+++ src/main/java/ladder/domain/ladder/Column.java
@@ -1,6 +1,39 @@
 package ladder.domain.ladder;
 
+import ladder.domain.ladder.footstep.FootStepStrategy;
+
+import java.util.Objects;
+
 //사다리 기둥
 public class Column {
-    // private Rung rung;
+     private final FootStep footStep;
+
+     private Column(final FootStepStrategy footStepStrategy) {
+          this.footStep = FootStep.init(footStepStrategy);
+     }
+
+     public Column(final FootStep footStep) {
+          this.footStep = footStep;
+     }
+
+     public static Column init(final FootStepStrategy footStepStrategy) {
+          return new Column(footStepStrategy);
+     }
+
+     public Column next(FootStepStrategy footStepStrategy) {
+          return new Column(footStep.next(footStepStrategy));
+     }
+
+     @Override
+     public boolean equals(Object o) {
+          if (this == o) return true;
+          if (o == null || getClass() != o.getClass()) return false;
+          Column column = (Column) o;
+          return footStep == column.footStep;
+     }
+
+     @Override
+     public int hashCode() {
+          return Objects.hash(footStep);
+     }
 }
--- src/test/java/ladder/util/Static.java
+++ src/test/java/ladder/util/Static.java
@@ -1,10 +1,15 @@
 package ladder.util;
 
+import ladder.domain.ladder.Column;
 import ladder.domain.ladder.footstep.FootStepStrategy;
 
 public class Static {
     public static final FootStepStrategy TRUE_RETURN_STRATEGY = () -> true;
     public static final FootStepStrategy FALSE_RETURN_STRATEGY = () -> false;
+
+    public static final Column COLUMN_WITH_RIGHT_FOOTSTEP = Column.init(TRUE_RETURN_STRATEGY);
+    public static final Column COLUMN_WITH_LEFT_FOOTSTEP = COLUMN_WITH_RIGHT_FOOTSTEP.next(TRUE_RETURN_STRATEGY);
+    public static final Column COLUMN_WITHOUT_FOOTSTEP = Column.init(FALSE_RETURN_STRATEGY);
 
     private Static() {}
 }
