f074b3688121f3f9747809e68e733d0a0c890d8c
nokchax <nokchax@gmail.com>
2020-05-27 08:06:20 +0900

    test : column's last method test

--- src/test/java/ladder/domain/ladder/ColumnTest.java
+++ src/test/java/ladder/domain/ladder/ColumnTest.java
@@ -25,7 +25,7 @@ class ColumnTest {
 
     @ParameterizedTest
     @MethodSource
-    @DisplayName("기둥의 다음 기둥생성 테스트")
+    @DisplayName("기둥의 다음 기둥 생성 테스트")
     void next(final Column column, final FootStepStrategy footStepStrategy, final Column expected) {
         assertThat(column.next(footStepStrategy)).isEqualTo(expected);
     }
@@ -38,6 +38,21 @@ class ColumnTest {
                 Arguments.of(COLUMN_WITH_LEFT_FOOTSTEP, FALSE_RETURN_STRATEGY, COLUMN_WITHOUT_FOOTSTEP),
                 Arguments.of(COLUMN_WITHOUT_FOOTSTEP, TRUE_RETURN_STRATEGY, COLUMN_WITH_RIGHT_FOOTSTEP),
                 Arguments.of(COLUMN_WITHOUT_FOOTSTEP, FALSE_RETURN_STRATEGY, COLUMN_WITHOUT_FOOTSTEP)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    @DisplayName("마지막 기둥 생성 테스트")
+    void last(final Column column, final Column expected) {
+        assertThat(column.last()).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> last() {
+        return Stream.of(
+                Arguments.of(COLUMN_WITH_LEFT_FOOTSTEP, COLUMN_WITHOUT_FOOTSTEP),
+                Arguments.of(COLUMN_WITH_RIGHT_FOOTSTEP, COLUMN_WITH_LEFT_FOOTSTEP),
+                Arguments.of(COLUMN_WITHOUT_FOOTSTEP, COLUMN_WITHOUT_FOOTSTEP)
         );
     }
 }
