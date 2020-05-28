8f3395edc821d016561255c21bc0a4b40b334dd7
nokchax <nokchax@gmail.com>
2020-05-28 13:11:00 +0900

    feat : add prizes string array from console input

--- src/main/java/ladder/LadderApplication.java
+++ src/main/java/ladder/LadderApplication.java
@@ -10,6 +10,7 @@ import java.util.List;
 public class LadderApplication {
     public static void main(String[] args) {
         List<String> players = ConsoleInput.inputPlayers();
+        List<String> prizes = ConsoleInput.inputPrizes();
         int ladderHeight = ConsoleInput.inputHeight();
 
         LadderGame ladderGame = LadderGame.init(LadderGameInitInfo.init(players, ladderHeight));
